04bfd531916a08b4a21c8a0b06a43532f8c80f6b
upend1310 <shawkumarupendra@outlook.com>
2023-02-18 23:53:05 -0500

    Updated comp as for React 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import ReactDOM from 'react-dom/client'
+// import ReactDOM from 'react-dom/client'
+// import Dashboard from './components/Dashboard'
+
+// function App() {
+//     return <Dashboard />
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+
+// root.render(<App />)
+
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+
 import Dashboard from './components/Dashboard'
 
-function App() {
-    return <Dashboard />
-}
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement)
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(<App />)
+root.render(
+    <StrictMode>
+        <Dashboard />
+    </StrictMode>
+)
